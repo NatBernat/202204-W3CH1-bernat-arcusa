7d49096ac7c517d7244adab7fe1cf6395c238163
Bernat Arcusa <bernat.arcusa@gmail.com>
2022-04-18 20:55:18 +0200

    Main class created

---
+++ js/index.js
@@ -0,0 +1,7 @@
+import Personaje from "./components/Personaje";
+
+const joffreyBaratheon = new Personaje("Joffrey", "Baratheon", 16);
+
+const nuevoPersonaje = (personaje) => personaje;
+
+nuevoPersonaje(joffreyBaratheon);
