41f7efea56b6a9ff777e3381863ca85230af1ae0
Bernat Arcusa <bernat.arcusa@gmail.com>
2022-04-18 21:15:58 +0200

    All class files and content created, also relations between them

--- js/index.js
+++ js/index.js
@@ -1,7 +1,16 @@
-import Personaje from "./components/Personaje";
+import Rey from "./components/Rey";
+import Luchador from "./components/Luchador";
+import Asesor from "./components/Asesor";
+import Escudero from "./components/Escudero";
 
-const joffreyBaratheon = new Personaje("Joffrey", "Baratheon", 16);
+const joffrey = new Rey("Joffrey", "Baratheon", 16, 2);
+const jaime = new Luchador("Jaime", "Lannister", 45, "Espada", 7);
+const daenerys = new Luchador("Daenerys", "Targaryen", 30, "Dragones", 10);
+const tyrion = new Asesor("Tyrion", "Lannister", 40, daenerys);
+const bronn = new Escudero("Bronn", "AguasNegras", 54, 0, jaime);
 
-const nuevoPersonaje = (personaje) => personaje;
-
-nuevoPersonaje(joffreyBaratheon);
+console.log(joffrey);
+console.log(jaime);
+console.log(daenerys);
+console.log(tyrion);
+console.log(bronn);
