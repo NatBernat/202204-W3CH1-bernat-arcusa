c75cdfac372adcc0d9ff61f86517b5510e06c4c4
Bernat Arcusa <bernat.arcusa@gmail.com>
2022-04-20 00:41:28 +0200

    Added king and fighter characters content on cards

--- js/components/Ficha.js
+++ js/components/Ficha.js
@@ -1,4 +1,6 @@
 import Component from "./Component.js";
+import Rey from "./Rey.js";
+import Luchador from "./Luchador.js";
 
 class Ficha extends Component {
   constructor(parentElement, personaje, nameClass) {
@@ -6,33 +8,46 @@ class Ficha extends Component {
     this.class = nameClass;
     this.personaje = personaje;
 
-    this.render();
+    this.render(personaje);
   }
 
   render() {
     this.element.innerHTML = `
     <div class="card character__card">
             <img
-              src="img/no-one.jpg"
-              alt="Nombre y familia del personaje"
-              class="character__picture card-img-top"
-            />
+            src="img/${this.personaje.nombre}.jpg"
+            alt="${this.personaje.nombre} ${this.personaje.familia}"
+            class="character__picture card-img-top"
+          />
             <div class="card-body">
-              <h2 class="character__name card-title h4">Nombre y familia</h2>
+              <h2 class="character__name card-title h4">${
+                this.personaje.nombre
+              } ${this.personaje.familia}</h2>
               <div class="character__info">
                 <ul class="list-unstyled">
-                  <li>Edad: X años</li>
+                  <li>Edad: ${this.personaje.edad} años</li>
                   <li>
                     Estado:
-                    <i class="fas fa-thumbs-down"></i>
-                    <i class="fas fa-thumbs-up"></i>
+                    ${
+                      this.personaje.vivo
+                        ? '<i class="fas fa-thumbs-up"></i>'
+                        : '<i class="fas fa-thumbs-down"></i>'
+                    }
                   </li>
                 </ul>
               </div>
               <div class="character__overlay">
                 <ul class="list-unstyled">
-                  <li>Años de reinado: X</li>
-                  <li>Arma: XXX</li>
+                  ${
+                    this.personaje instanceof Rey
+                      ? `<li>Años de reinado: ${this.personaje.anyosReinado}</li>`
+                      : ""
+                  }
+                  ${
+                    this.personaje instanceof Luchador
+                      ? `<li>Arma: ${this.personaje.arma}</li>`
+                      : ""
+                  }
                   <li>Destreza: X</li>
                   <li>Peloteo: X</li>
                   <li>Asesora a: X</li>
